f99c3d6686a6a3b9b01c38226b7b13952e476917
Rahul Suresh <rahul.tech.work@gmail.com>
2019-07-04 16:01:21 +0530

    5.0.4

--- android/app/src/main/java/com/rahul/stb/MainActivity.java
+++ android/app/src/main/java/com/rahul/stb/MainActivity.java
@@ -4,11 +4,23 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 import android.view.WindowManager.LayoutParams;
+import android.view.WindowManager;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    getWindow().addFlags(LayoutParams.FLAG_SECURE);
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
     GeneratedPluginRegistrant.registerWith(this);
   }
+
+
+@Override
+protected void onPause() {
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    super.onPause();
 }
+  
+}
+
